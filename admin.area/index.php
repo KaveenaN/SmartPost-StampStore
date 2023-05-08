f41ad7bf71c34735a518a5a997ed86e3a2e51828
Kaveena <kaveenank@gmail.com>
2023-05-08 15:05:26 +0530

    First Commit

---
+++ admin.area/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+
+    <!-- bootstrap css link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        intergrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+        crossorigin="anonymous">
+
+
+      <!-- font awesome link -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" 
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+   <!-- css file -->
+   <link rel="stylesheet" href="../style.css">
+   <style>
+    .admin_image{
+    width: 100px;
+    object-fit: contain;
+}
+
+   .footer{
+    position:absolute;
+    bottom: 0;
+   }
+
+    </style>
+</head>
+<body>
+    <!-- navbar -->
+    <div class="container-fluid p-0">
+        <!-- first child -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-info">
+            <div class="container-fluid">
+                <img src="../images/logo.png" alt="" class="logo"> 
+               <nav class="navbar navbar-expand-lg ">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="" class="nav-link">Welcome Guest</a>
+                    </li>
+                </ul>
+
+               </nav>
+
+            </div>
+        </nav>
+
+        <!-- second child -->
+        <div class="bg-light">
+            <h3 class="text-center p-2">Manage Details</h3>
+        </div>
+
+        <!-- third child -->
+        <div class="row">
+            <div class="col-md-12 bg-secondary p-1 d-flex align-items-center">
+                <div class="p-3">
+                    <a href="#"> <img src="../images/admin.png" alt="" class="admin_image"></a>
+                    <p class="text-light text-center">Admin name</p>
+                </div>
+
+                <div class="button text-center">
+                    <button class="my-3"><a href="index.php?insert_category" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">View Categories</a></button>
+                    <button><a href="index.php?insert_search" class="nav-link text-light bg-info my-1">Insert Search</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">View Search</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">Insert Special Covers</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">View Special Covers</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">All Orders</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">All Payments</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">List Users</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">Log Out</a></button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!-- fourth child-->
+<div class="container my-5">
+    <?php
+    if(isset($_GET['insert_category'])){
+        include('insert_categories.php');
+    }
+    if(isset($_GET['insert_search'])){
+        include('insert_search.php');
+    }
+    
+    ?>
+</div>    
+
+<!-- last child -->
+<div class="bg-info p-3 text-center footer">
+            <p>Copyright © 2023 SmartPost. All Rights Reserved</p>
+        </div> 
+<!-- bootstrap js link -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
+     integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" 
+     crossorigin="anonymous"></script> 
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" contents="IE=edge">
+        <meta name="viewport" content="width=device-width, initial scale=1.0">
+        <title>Stamp Store</title>
+        <!-- bootstrap css link -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        intergrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+        crossorigin="anonymous">
+        <!-- font awesome link -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" 
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- css file -->
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <!-- navbar -->
+        <div class="container-fluid p-0">
+            <!-- first child -->
+            <nav class="navbar navbar-expand-lg bg-info">
+  <div class="container-fluid">
+
+    <img src="./images/logo.png" alt="" class="logo">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"><i class="fa fa-home" aria-hidden="true"></i>Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fa-solid fa-envelopes-bulk"></i>Stamps</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fa fa-phone" aria-hidden="true"></i>Contact Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i>My Cart</a>
+        </li>
+       
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-light" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<!-- second child -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+    <ul class="navbar-nav me-auto">
+    <li class="nav-item">
+          <a class="nav-link" href="#">Welcome Guest</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+
+    </ul>
+</nav>
+
+<!-- third child -->
+<div class="bg-light">
+    <h3 class="text-center">Stamp Store</h3>
+    <p class="text-center">Department of Posts
+Sri Lanka</p>
+</div>
+
+<!-- fourth child -->
+<div class="row">
+    <div class="col-md-10">
+        <!-- products -->
+        <div class="row">
+            <div class="col-md-4 mb-2">
+                <div class="card">
+                    <img src="./images/vesak1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-info">Add to cart</a>
+                        <a href="#" class="btn btn-secondary">View more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-2">
+                <div class="card">
+                    <img src="./images/vesak1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-info">Add to cart</a>
+                        <a href="#" class="btn btn-secondary">View more</a>
+                    </div>
+                </div>
+            </div>   
+            <div class="col-md-4 mb-2">
+                <div class="card">
+                    <img src="./images/vesak1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-info">Add to cart</a>
+                        <a href="#" class="btn btn-secondary">View more</a>
+                    </div>
+                </div>
+            </div>    
+        </div>
+        <div class="row">
+            <div class="col-md-4 mb-2">
+                <div class="card">
+                    <img src="./images/vesak1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-info">Add to cart</a>
+                        <a href="#" class="btn btn-secondary">View more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-2">
+                <div class="card">
+                    <img src="./images/vesak1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-info">Add to cart</a>
+                        <a href="#" class="btn btn-secondary">View more</a>
+                    </div>
+                </div>
+            </div>   
+            <div class="col-md-4 mb-2">
+                <div class="card">
+                    <img src="./images/vesak1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-info">Add to cart</a>
+                        <a href="#" class="btn btn-secondary">View more</a>
+                    </div>
+                </div>
+            </div>    
+        </div>
+        <div class="row">
+            <div class="col-md-4 mb-2">
+                <div class="card">
+                    <img src="./images/vesak1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-info">Add to cart</a>
+                        <a href="#" class="btn btn-secondary">View more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-2">
+                <div class="card">
+                    <img src="./images/vesak1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-info">Add to cart</a>
+                        <a href="#" class="btn btn-secondary">View more</a>
+                    </div>
+                </div>
+            </div>   
+            <div class="col-md-4 mb-2">
+                <div class="card">
+                    <img src="./images/vesak1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-info">Add to cart</a>
+                        <a href="#" class="btn btn-secondary">View more</a>
+                    </div>
+                </div>
+            </div>    
+        </div>
+    </div>    
+    <div class="col-md-2  bg-secondary p-0">
+        <!-- categories -->
+        <ul class="navbar-nav me-auto text-center">
+            <li class="nav-item bg-info">
+                <a href="#" class="nav-link text-light"><h4>Categories</h4></a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light">Stamps</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light">Miniature Sheets</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link text-light">First Day Covers</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link text-light">Special Covers</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link text-light">Others</a>
+        </li>
+       </ul>
+
+       <!-- search -->
+
+       <ul class="navbar-nav me-auto text-center">
+        <li class="nav-item bg-info">
+            <a href="#" class="nav-link text-light"><h4>search</h4></a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link text-light">Years</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link text-light">Price Range</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link text-light">Key words</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link text-light">Check order details</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link text-light">Payment Methods</a>
+        </li>
+       </ul>
+
+        
+
+    </div>
+</div>
+
+<!-- last child -->
+       <div class="bg-info p-3 text-center">
+            <p>Copyright © 2023 SmartPost. All Rights Reserved</p>
+        </div> 
+
+        </div>
+
+    <!-- bootstrap js link -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
+     integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" 
+     crossorigin="anonymous"></script> 
+    </body>
+</html>
